c1aa5e215f09d36bc00d90a9c36470c4e77c7e5e
Антон Кондратов <contact@site-home.ru>
2017-10-10 19:09:08 +0300

    Поправил путь к конфигу

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ import os
 
 from podcast_downloader import PodcastDownloader
 
-if os.path.exists('config.py'):
+if os.path.exists(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.py')):
     from config import config
 else:
     from config_sample import config
